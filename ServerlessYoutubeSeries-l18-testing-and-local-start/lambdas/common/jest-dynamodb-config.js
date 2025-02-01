b27b7f8601f366226aa8edfa88ec7a5f5e3443cf
kayodebello <kayafeez@gmail.com>
2025-02-01 12:34:45 -0600

    adding CI config and renaming table

---
+++ ServerlessYoutubeSeries-l18-testing-and-local-start/lambdas/common/jest-dynamodb-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    tables: [
+        {
+            "TableName": "player-points-table-circle-ci",
+            "KeySchema": [
+                {
+                     "AttributeName": "ID",
+                     "KeyType": "HASH"
+                }
+                
+            ],
+            "AttributeDefinitions": [
+                {
+                     "AttributeName": "ID",
+                     "AttributeType": "S",
+                },
+            ],
+            BillingMode: 'PAY_PER_REQUEST',
+            },
+    ],
+    
+};
+        
